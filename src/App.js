93f4fdec2a0681e510ae7fda3250df2bd0a30fb2
Eula Ludmila <eula.silva.teixeira@gmail.com>
2022-02-12 10:40:12 -0300

    feat: parte 1 finalizado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './styles/global.scss';
+import eu from './assets/images/eu.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <header></header>
+      <section className='content-infos'>
+        <figure>
+          <img src={eu} alt="Eula"/>
+        </figure>
+        <div className='infos-name'>
+          <h1>Eula Ludmila</h1>
+          <h2>Desenvolvedora Front-End Júnior</h2>
+          <p className='infos-idade'>20 anos</p>
+          <p className='infos-idade'>Brasileira - Solteira</p>
+          <p className='infos-habilidades'>React | HTLM | CSS | JavaScript | PHP | Java | MySql</p>
+        </div>
+      </section>
+      <main>
+        <section className='content-about'>
+          <h2>Sobre</h2>
+          <p className='about-text'>
+            Me chamo Eula e tenho 20 anos e ingressei na área de tecnologia a partir do curso Técnico de Informática que conclui em 2019 no SENAI e até então não larguei mais essa área. Atualmente trabalho como Desenvolvedora Fron-End Jr. e estou cursando Análise e Desenvolvimento de Sistemas na Fatec Carapicuíba.
+          </p>
+        </section>
+        {/* <section className='content-about'>
+          <h2>Projetos e Participações</h2>
+          <p className='about-text'>
+            Me chamo Eula e tenho 20 anos e ingressei na área de tecnologia a partir do curso Técnico de Informática que conclui em 2019 no SENAI e até então não larguei mais essa área. Atualmente trabalho como Desenvolvedora Fron-End Jr. e curso Análise e Desenvolvimento de Sistemas na Fatec Carapicuíba.
+          </p>
+        </section>
+        <section className='content-about'>
+          <h2>Histórico</h2>
+          <div className='grid-historico'>
+            <div>
+              <h3>Empresa</h3>
+              <span>01/2020 | até o presente</span>
+
+              <ul>
+                <li>Teste</li>
+                <li>Teste</li>
+                <li>Teste</li>
+              </ul>
+            </div>
+          </div>
+        </section> */}
+      </main>
+    </>
   );
 }
 
